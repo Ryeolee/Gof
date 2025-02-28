db0b5db6c0fbcb7576759c29fa24ab570b781f17
seungryeol <opop0421@naver.com>
2025-03-01 02:36:01 +0900

    싱글톤 코딩

--- GRIMPAN/src/grimpan.ts
+++ GRIMPAN/src/grimpan.ts
@@ -1,4 +1,14 @@
+/**
+ * 하나의 인스턴스를 사용함. 메모리 낭비를 줄일 수 있음.
+ * 단점
+ * 1. 강한 결합을 하게 됨.
+ * 2. 생성자가 private이라서 단위 테스트가 힘듦
+ * 3. getInstance()함수가 단일 책임의 원칙을 어김. -> 바뀜 이유가 한가지여야한다. ex) 다른 형식의 그림판을 만들고 싶을 경우 #canvas가 다른 형식으로 만들어질 수 있음.
+ *
+ */
+
 class Grimpan {
+  private static instance: Grimpan;
   constructor(canvas: HTMLElement | null) {
     if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
       throw new Error("canvas 엘리먼트를 입력하세요");
@@ -7,6 +17,13 @@ class Grimpan {
 
   initialize() {}
   initializeMenu() {}
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Grimpan(document.querySelector("#canvas"));
+    }
+    return this.instance;
+  }
 }
 
-export default new Grimpan(document.querySelector("#canvas"));
+export default Grimpan;
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,2 @@
-import g1 from "./grimpan.js";
-import g2 from "./grimpan.js";
-console.log(g1 === g2);
+import Grimpan from "./grimpan.js";
+console.log(Grimpan.getInstance() === Grimpan.getInstance());
