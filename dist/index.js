ebf93982f23719e63b40a207e313be7d7d5374b8
seungryeol <opop0421@naver.com>
2025-03-01 11:59:45 +0900

    ..

--- dist/index.js
+++ dist/index.js
@@ -2,32 +2,31 @@ import AbstractGrimpanFactory from "./AbstractGrimpanFactory.js";
 import ChromeGrimpan from "./ChromeGrimpan.js";
 import IEGrimpan from "./IEGrimpan.js";
 // 단일 책임 원칙을 어김.
+// 개방 폐쇄의 원칙을 어김.
 function grimpanFactory(type) {
-    if (type === "ie") {
-        return IEGrimpan.getInstance();
-    }
-    else if (type === "chrome") {
-        return ChromeGrimpan.getInstance();
-    }
-    else {
-        throw new Error("에러");
-    }
+  if (type === "ie") {
+    return IEGrimpan.getInstance();
+  } else if (type === "chrome") {
+    return ChromeGrimpan.getInstance();
+  } else {
+    throw new Error("에러");
+  }
 }
 class ChromeGrimpanFactory extends AbstractGrimpanFactory {
-    static createGrimpan() {
-        return ChromeGrimpan.getInstance();
-    }
+  static createGrimpan() {
+    return ChromeGrimpan.getInstance();
+  }
 }
 class IEGrimpanFactory extends AbstractGrimpanFactory {
-    static createGrimpan() {
-        return IEGrimpan.getInstance();
-    }
+  static createGrimpan() {
+    return IEGrimpan.getInstance();
+  }
 }
 function main() {
-    // grimpanFactory("ie");
-    // grimpanFactory("chrome");
-    const grimpan = ChromeGrimpanFactory.createGrimpan();
-    grimpan.initialize();
-    grimpan.initializeMenu();
+  // grimpanFactory("ie");
+  // grimpanFactory("chrome");
+  const grimpan = ChromeGrimpanFactory.createGrimpan();
+  grimpan.initialize();
+  grimpan.initializeMenu();
 }
 main();
