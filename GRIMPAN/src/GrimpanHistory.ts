e87f1b4d497c3282a5c7290fb3dcf014c008181e
seungryeol <opop0421@naver.com>
2025-03-01 15:17:33 +0900

    프로토타입 패턴

--- GRIMPAN/src/GrimpanHistory.ts
+++ GRIMPAN/src/GrimpanHistory.ts
@@ -2,10 +2,29 @@ import Grimpan from "./AbstractGrimpans";
 import ChromeGrimpan from "./ChromeGrimpan";
 import IEGrimpan from "./IEGrimpan";
 
+interface Clonable {
+  clone(): Clonable;
+}
+class HistoryStack extends Array {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
+  }
+
+  getStack() {
+    return this.stack.clone();
+  }
+
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
   }
 
   abstract initialize(): void;
